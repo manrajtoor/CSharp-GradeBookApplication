e74dd91242228cae36976df1293f9945905454ae
manrajtoor <manrajt@umich.edu>
2020-11-30 13:32:20 -0500

    Attempt at RankedGradeBook implementation

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,44 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+    
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Need at least 5 students");
+            }
+
+            // Organize student grades
+            List<double> grades = new List<double>();
+
+            foreach (var student in Students)
+            {
+                grades.Add(student.AverageGrade);
+            }
+            grades.Sort();
+            // Find which percentile score is in
+            List<char> grade = new List<char>() { 'A', 'B', 'C', 'D' };
+            double percentile = .8 * Students.Count;
+            double unitDrop = .2 * Students.Count;
+            int numDrops = 0;
+            while(true)
+            {
+                if (percentile <= 0)
+                {
+                    return 'F';
+                }
+                // Check initial index
+                if (averageGrade >= grades[(int)percentile])
+                {
+                    // If grade is within range, assign grade and break
+                    return grade[numDrops];
+                }
+                // Update index and 
+                numDrops += 1;
+                percentile = percentile - unitDrop;
+            }
+                    
+        }
     }
 }
